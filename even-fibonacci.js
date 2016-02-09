b673550c6213dd94dd97d1c62aa59810be88c7bb
Greg Turmel <gregturmel@gmail.com>
2016-02-09 09:51:16 -0500

    format for readability

--- even-fibonacci.js
+++ even-fibonacci.js
@@ -1,8 +1,8 @@
-var1=0;
-var2=1;
+var var1 = 0;
+var var2 = 1;
 evenFibonacci = [];
 
-for(num=1; num <=60; num++) {
+for(num = 1; num <= 60; num ++) {
     var3 = var1+var2;
     if (var3 < 4000000 && var3 % 2 === 0){
     evenFibonacci.push(var3)};
