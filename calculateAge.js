be0d220e8be02681b4207e3034227211b002fd9d
Greg Turmel <gregturmel@gmail.com>
2016-02-10 09:54:34 -0500

    initial calculateAge.js

---
+++ calculateAge.js
@@ -0,0 +1,17 @@
+// Write a function named calculateAge that:
+// takes 2 arguments: birth year, current year.
+// calculates the 2 possible ages based on those years.
+// outputs the result to the screen: "You are either NN or NN"
+// call the function three times with different sets of values.
+
+function (one number, second number) {
+   age1 = one number - second number
+   age2 = one number - second number - 1
+   You are either age1 or age2
+}
+
+function calculateAge(birthYear, currentYear) {
+   var age1 = currentYear - birthYear;
+   var age2 = currentYear - birthYear - 1;
+   console.log("You are either " + age1 + " or " + age2);
+};
