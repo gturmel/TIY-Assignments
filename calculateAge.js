99f5391ac8d97e0f4aeb50e96792055019cbe6eb
Greg Turmel <gregturmel@gmail.com>
2016-02-10 09:57:58 -0500

    add negative value response

--- calculateAge.js
+++ calculateAge.js
@@ -10,8 +10,14 @@ function (one number, second number) {
    You are either age1 or age2
 }
 
+
 function calculateAge(birthYear, currentYear) {
    var age1 = currentYear - birthYear;
    var age2 = currentYear - birthYear - 1;
+   if (age1 < 0 || age2 < 0 ){
+       console.log("You haven't been born yet!")
+   }
+   else if
+
    console.log("You are either " + age1 + " or " + age2);
 };
