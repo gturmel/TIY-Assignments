04db07f11db8cda0dd3a67800c8c01f079be5926
Greg Turmel <gregturmel@gmail.com>
2016-02-11 10:07:48 -0500

    snack-supply initial commit

---
+++ snack-supply.js
@@ -0,0 +1,24 @@
+// ever wonder how much a lifetime supply of your favorite snack is? Wonder no more
+
+// write a fxn named calculate Supply that
+// takes two arguments, age, amnt/day
+// cals the amnt consumed for rest of the life (constant max age)
+// onputs "you will need NN to last you until the ripe old age of x
+// call 3x, using different values each time"
+
+var age = 32;
+var totalAge = 75;
+var amountPerDay = 5;
+var totalAmount = 0;
+
+function calculateSupply (age, amountPerDay) {
+    for (age; age <= 75; age ++) {
+        amountPerDay = age * 365 * amountPerDay;
+        totalAmount = amountPerDay + totalAmount;
+        console.log(totalAmount);
+    };
+};
+
+console.log("You will need " + totalAmount + " M&M's to last you until the ripe old age of " + totalAge + ".");
+
+console.log(calculateSupply(1, 2));
