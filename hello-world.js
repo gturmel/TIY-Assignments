79be89fdfea4b16f1601fcdbf9191a0f079cac22
Greg Turmel <gregturmel@gmail.com>
2016-02-17 10:33:37 -0500

    add Hello World

---
+++ hello-world.js
@@ -0,0 +1,20 @@
+// write a function named helloWorld that:
+// takes 1 argument, a lang code (eg "es", "de", "en")
+// returns "hello world" for the given lang, for at least 3 lang
+// default to returning english.
+// call that function for each supported lang, log result
+
+function helloWorld (i){
+    if(i = "es") {
+        console.log("Hola, Mundo");
+    }
+    else {
+        console.log("Hello, World")
+    }
+
+
+};
+
+
+helloWorld("es");
+helloWorld("yep");
