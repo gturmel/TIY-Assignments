e56c24c3eadc9b62a3836eb02fa19127c82a3e97
Greg Turmel <gregturmel@gmail.com>
2016-02-25 10:40:06 -0500

    add array-challange.js.

---
+++ array-challange.js
@@ -0,0 +1,3 @@
+// create an array to hold your top choices (whatever you want)
+// for each choice, log to the screen a string like: #my #1 choice is blue.
+// bonus - change it to log "1st choice, 2nd choice" based on what number it is.
