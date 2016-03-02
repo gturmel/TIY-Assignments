b43c52a8925bfa910322c9963ba955355b8de61c
Greg Turmel <gregturmel@gmail.com>
2016-03-02 10:28:37 -0500

    add even-count.js

---
+++ even-count.js
@@ -0,0 +1,13 @@
+// write a for loop that goes from 0 to 21044
+//     for each iteration, check if current number is even or odd, and report that to screen
+
+for(var num = 0; num <= 21044; num +=2){
+    console.log(num + " is even");
+};
+
+
+for (var i = 0; i <= 21044; i++){
+    if(i % 2 === 0) {
+        console.log(i + " is even");
+    }
+};
